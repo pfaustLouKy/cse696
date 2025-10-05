eed5f1e29e1f48f2f33fed3c54cc403f39e95023
Paul Faust <pbfaus01@louisville.edu>
2025-10-04 20:31:25 -0400

    first commit

---
+++ build.py
@@ -0,0 +1,6 @@
+from qiskit_ibm_runtime import QiskitRuntimeService
+ 
+# If you named your credentials, optionally specify the name here, as follows:
+# QiskitRuntimeService(name='account-name')
+# If you don't specify a name, the default credentials are loaded.
+service = QiskitRuntimeService()
